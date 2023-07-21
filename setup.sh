c48fe588099e85aa966e96df18a12d318c1fedd4
James Gundlach <james.gundlach79@gmail.com>
2023-07-21 06:15:09 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -115,7 +115,7 @@ sed -i '327s/termux/proot/' ../usr/var/lib/proot-distro/installed-rootfs/debian/
 
 #Set aliases
 echo "
-alias debian='proot-distro login debian --user $username -shared-tmp && clear'
+alias debian='proot-distro login debian --user $username --shared-tmp && clear'
 alias virgl='GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=3.3 $@'
 alias ls='exa -lF'
 alias cat='bat $@'
