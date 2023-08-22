3054099f51e6d204c1fd3537a5f979355687ecbd
James Gundlach <james.gundlach79@gmail.com>
2023-08-22 06:46:02 -0400

    Add files via upload

---
+++ proot-distro/slackware.sh
@@ -0,0 +1,8 @@
+# This is a default distribution plug-in.
+# Do not modify this file as your changes will be overwritten on next update.
+# If you want customize installation, please make a copy.
+
+DISTRO_NAME="Slackware"
+
+TARBALL_URL['aarch64']="https://slackware.uk/slackwarearm/slackwarearm-devtools/minirootfs/roots/slackaarch64-current-miniroot_08Aug23.tar.xz"
+TARBALL_SHA256['aarch64']="3f41c9b0774e0bd73ed030956183740158d46223aa7ca76dcc69249210a86dea"
