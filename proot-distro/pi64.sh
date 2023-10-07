5a04261227647b13d65d8bd07263263f5267023c
James Gundlach <james.gundlach79@gmail.com>
2023-10-07 14:21:31 +0000

    .

--- proot-distro/pi64.sh
+++
@@ -1,7 +0,0 @@
-# This is a default distribution plug-in.
-# Do not modify this file as your changes will be overwritten on next update.
-# If you want customize installation, please make a copy.
-DISTRO_NAME="Raspberry Pi OS aarch64"
-
-TARBALL_URL['aarch64']="http://downloads.raspberrypi.org/raspios_full_arm64/root.tar.xz"
-TARBALL_SHA256['aarch64']="e8fd091305558e1f8c1ff44c905e28618cac159afbaa9513420427fe98362256"
