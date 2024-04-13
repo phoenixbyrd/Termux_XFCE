8f9816b2f9e5c9e203e1ef26c18e5991e0c542c0
phoenixbyrd <james.gundlach79@gmail.com>
2024-04-13 07:24:02 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -250,13 +250,17 @@ cat <<'EOF' > $HOME/../usr/bin/kill_termux_x11
 
 # Check if Apt, dpkg, or Nala is running in Termux or Proot
 if pgrep -f 'apt|apt-get|dpkg|nala'; then
-  zenity --info --text="Software is currently installing in Termux or Proot. Please wait for this processes to finish before continuing."
+  zenity --info --text="Software is currently installing in Termux or Proot. Please wait for these processes to finish before continuing."
   exit 1
 fi
 
 # Get the process IDs of Termux-X11 and XFCE sessions
-termux_x11_pid=$(pgrep -f "/system/bin/app_process / com.termux.x11.Loader :1.0")
+termux_x11_pid=$(pgrep -f /system/bin/app_process.*com.termux.x11.Loader)
 xfce_pid=$(pgrep -f "xfce4-session")
+
+# Add debug output
+echo "Termux-X11 PID: $termux_x11_pid"
+echo "XFCE PID: $xfce_pid"
 
 # Check if the process IDs exist
 if [ -n "$termux_x11_pid" ] && [ -n "$xfce_pid" ]; then
@@ -272,6 +276,7 @@ pid=$(echo "$info_output" | grep -o 'TERMUX_APP_PID=[0-9]\+' | awk -F= '{print $
 kill "$pid"
 
 exit 0
+
 
 EOF
 
