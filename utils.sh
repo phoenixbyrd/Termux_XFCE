07ff946a58630f0f3e794e2249de96c92efa1a9e
phoenixbyrd <james.gundlach79@gmail.com>
2024-05-06 16:51:29 -0400

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -90,21 +90,33 @@ StartupNotify=false
 chmod +x $PREFIX/share/applications/cp2menu.desktop 
 
 #App Installer Utility .. For installing additional applications not available in Termux or Debian proot repositories. 
-
-cat <<'EOF' > $PREFIX/bin/app-installer
+cat <<'EOF' > "$PREFIX/bin/app-installer"
 #!/bin/bash
 
 # Define the directory paths
 INSTALLER_DIR="$HOME/.App-Installer"
 REPO_URL="https://github.com/phoenixbyrd/App-Installer.git"
 DESKTOP_DIR="$HOME/Desktop"
-APP_DESKTOP_FILE="$DESKTOP_DIR/App-Installer.desktop"
+APP_DESKTOP_FILE="$DESKTOP_DIR/app-installer.desktop"
 
 # Check if the directory already exists
 if [ ! -d "$INSTALLER_DIR" ]; then
     # Directory doesn't exist, clone the repository
     git clone "$REPO_URL" "$INSTALLER_DIR"
-    # Create the .desktop file
+    if [ $? -eq 0 ]; then
+        echo "Repository cloned successfully."
+    else
+        echo "Failed to clone repository. Exiting."
+        exit 1
+    fi
+else
+    echo "Directory already exists. Skipping clone."
+    "$INSTALLER_DIR/app-installer"
+fi
+
+# Check if the .desktop file exists
+if [ ! -f "$APP_DESKTOP_FILE" ]; then
+    # .desktop file doesn't exist, create it
     echo "[Desktop Entry]
     Version=1.0
     Type=Application
@@ -117,21 +129,19 @@ if [ ! -d "$INSTALLER_DIR" ]; then
     Terminal=false
     StartupNotify=false
 " > "$APP_DESKTOP_FILE"
-chmod +x "$APP_DESKTOP_FILE"
-chmod +x $HOME/.App-Installer/*
-    if [ $? -eq 0 ]; then
-        echo "Repository cloned successfully."
-    else
-        echo "Failed to clone repository. Exiting."
-        exit 1
-    fi
-else
-    echo "Directory already exists. Skipping clone."
-    $HOME/.App-Installer/./app-installer
+    chmod +x "$APP_DESKTOP_FILE"
 fi
-EOF
-chmod +x $PREFIX/bin/app-installer
 
+# Ensure the app-installer script is executable
+chmod +x "$INSTALLER_DIR/app-installer"
+
+EOF
+chmod +x "$PREFIX/bin/app-installer"
+bash $PREFIX/bin/app-installer
+
+# Check if the .desktop file exists
+if [ ! -f "$HOME/Desktop/app-installer.desktop" ]; then
+# .desktop file doesn't exist, create it
 echo "[Desktop Entry]
 Version=1.0
 Type=Application
@@ -143,8 +153,9 @@ Categories=System;
 Path=
 Terminal=false
 StartupNotify=false
-" > $HOME/Desktop/app-installer.desktop
-    chmod +x $HOME/Desktop/app-installer.desktop
+" > "$HOME/Desktop/app-installer.desktop"
+chmod +x "$HOME/Desktop/app-installer.desktop"
+fi
 
 #Start script
 cat <<'EOF' > start
