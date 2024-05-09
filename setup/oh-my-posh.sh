959e6a3d43bbe6fbf36af926fbe9039bca659aef
androc-o <ehfaz.kh@gmail.com>
2024-05-09 14:03:01 +0600

    867

--- setup/oh-my-posh.sh
+++ setup/oh-my-posh.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-DIR_TG="$HOME/termux-go"
+dir_tg="$HOME/termux-go"
 pkg install zsh git wget -y 
 if [ ! -d "$DIR_TG" ]; then
-  echo rm -rf $DIR_TG ; mkdir $DIR_TG;
+  echo rm -rf $dir_tg ; mkdir $dir_tg;
 else
-mkdir $DIR_TG
+mkdir $dir_tg
 fi
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v19.27.0/posh-linux-arm64 -O $DIR_TG/.oh-my-posh
-chmod +x $DIR_TG/.oh-my-posh
-wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Meslo/M/Regular/MesloLGMNerdFontPropo-Regular.ttf -O $DIR_TG/NerdFont.ttf
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v19.27.0/posh-linux-arm64 -O $dir_tg/.oh-my-posh
+chmod +x $dir_tg/.oh-my-posh
+wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Meslo/M/Regular/MesloLGMNerdFontPropo-Regular.ttf -O $dir_tg/NerdFont.ttf
 mv NerdFont.ttf ~/.termux/font.ttf
 termux-reload-settings
-#wget https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/bubbles.omp.json -O $DIR_TG.bubbles.omo.json
-#echo "eval "$(~/$DIR_TG.oh-my-posh init zsh --config ~/$DIR_TG/bubbles.omp.json)""  | tee -a ~/.zshrc > /dev/null
+#wget https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/bubbles.omp.json -O $dir_tg.bubbles.omo.json
+#echo "eval "$(~/$dir_tg.oh-my-posh init zsh --config ~/$dir_tg/bubbles.omp.json)""  | tee -a ~/.zshrc > /dev/null
 #source .zshrc
