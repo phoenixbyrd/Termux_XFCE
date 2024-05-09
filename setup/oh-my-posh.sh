f6efeea87397b270348043dd43c8d70aa56c0bff
androc-o <ehfaz.kh@gmail.com>
2024-05-09 13:16:50 +0600

    2

--- setup/oh-my-posh.sh
+++ setup/oh-my-posh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pkg install zsh git wget -y 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v19.27.0/posh-linux-arm64 -O .oh-myposh
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v19.27.0/posh-linux-arm64 -O .oh-my-posh
 chmod +x .oh-my-posh
 wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Meslo/M/Regular/MesloLGMNerdFontPropo-Regular.ttf 
 mv MesloLGMNerdFontPropo-Regular.ttf .termux/font.ttf
