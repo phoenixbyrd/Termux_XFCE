113577d70a15640a049ef4b0f3ea1f83b5a9c786
androc-o <ehfaz.kh@gmail.com>
2024-05-09 13:49:31 +0600

    add zsh

--- setup/oh-my-posh.sh
+++ setup/oh-my-posh.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+DIR_TG = ~/.termux/termux-go
 pkg install zsh git wget -y 
+if [ ! -d "$DIR_TG" ]; then
+  echo rm -rf $DIR_TG ; mkdir $DIR_TG;
+else
+mkdir $DIR_TG
+fi
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v19.27.0/posh-linux-arm64 -O .oh-my-posh
-chmod +x .oh-my-posh
-wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Meslo/M/Regular/MesloLGMNerdFontPropo-Regular.ttf 
-mv MesloLGMNerdFontPropo-Regular.ttf .termux/font.ttf
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v19.27.0/posh-linux-arm64 -O $DIR_TG/.oh-my-posh
+chmod +x $DIR_TG/.oh-my-posh
+wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Meslo/M/Regular/MesloLGMNerdFontPropo-Regular.ttf -O $DIR_TG/NerdFont.ttf
+mv NerdFont.ttf ~/.termux/font.ttf
 termux-reload-settings
-#wget https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/bubbles.omp.json -O .bubbles.omo.json
-#echo "eval "$(./.oh-my-posh init zsh --config ~/bubbles.omp.json)""  | tee -a ~/.zshrc > /dev/null
+#wget https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/bubbles.omp.json -O $DIR_TG.bubbles.omo.json
+#echo "eval "$(~/$DIR_TG.oh-my-posh init zsh --config ~/$DIR_TG/bubbles.omp.json)""  | tee -a ~/.zshrc > /dev/null
 #source .zshrc
